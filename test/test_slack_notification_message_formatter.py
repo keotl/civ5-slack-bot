9e233cb1b91ce71c7ed248bde93c7dc6e5915de2
Kento A. Lauzon <kento.lauzon@ligature.ca>
2020-03-21 20:31:42 -0400

    test hide player

--- test/test_slack_notification_message_formatter.py
+++ test/test_slack_notification_message_formatter.py
@@ -1,9 +1,11 @@
 import unittest
-from unittest import mock
 
+from jivago.serialization.object_mapper import ObjectMapper
+
+from civbot.app.resource.civhook.civ_hook_model import CivHookStateModel
 from civbot.app.slack.slack_notification_message_formatter import (
     SlackNotificationMessageFormatter)
-from test.dummy_entities import SOME_GAME_STATE, SOME_PLAYER
+from test.utils.dummy_entities import SOME_GAME_STATE, SOME_PLAYER
 
 
 class SlackNotificationMessageFormatterTests(unittest.TestCase):
@@ -16,7 +18,20 @@ class SlackNotificationMessageFormatterTests(unittest.TestCase):
 
         self.assertTrue(f"{SOME_PLAYER.nickName}" in message)
 
+    def test_hides_players_whose_turn_has_ended(self):
+        state = clone(SOME_GAME_STATE)
+        state.players[0].isTurnComplete = True
+
+        message = self.formatter.format_message(state)
+
+        self.assertFalse(f"{SOME_PLAYER.nickName}" in message)
+
     def test_shows_turn_number(self):
         message = self.formatter.format_message(SOME_GAME_STATE)
 
         self.assertTrue(str(SOME_GAME_STATE.gameTurn) in message)
+
+def clone(state: CivHookStateModel)-> CivHookStateModel:
+    object_mapper = ObjectMapper()
+    return object_mapper.deserialize(object_mapper.serialize(state), CivHookStateModel)
+

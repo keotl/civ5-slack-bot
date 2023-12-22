b55699bb8ececefdcc5772ff7992d461f33f51e6
Kento Lauzon <kento@lauzon.co.uk>
2023-12-22 13:40:52 +0000

    fallback to slack notifier as default

--- civbot/main.py
+++ civbot/main.py
@@ -4,7 +4,6 @@ from jivago.config.production_jivago_context import ProductionJivagoContext
 from jivago.jivago_application import JivagoApplication
 
 import civbot.app
-from civbot.app.discord.discord_client import DiscordClient
 from civbot.app.discord.discord_game_notifier import DiscordGameNotifier
 from civbot.app.service.game_state_notifier import GameStateNotifier
 from civbot.app.slack.slack_state_notifier import SlackStateNotifier
@@ -14,7 +13,7 @@ class SlackContext(ProductionJivagoContext):
 
     def configure_service_locator(self):
         super().configure_service_locator()
-        if os.environ.get("NOTIFIER") == "slack":
+        if os.environ.get("NOTIFIER") in ("slack", None):
             self.service_locator().bind(GameStateNotifier, SlackStateNotifier)
         if os.environ.get("NOTIFIER") == "discord":
             self.service_locator().bind(GameStateNotifier, DiscordGameNotifier)
@@ -23,5 +22,4 @@ class SlackContext(ProductionJivagoContext):
 application = JivagoApplication(civbot.app, context=SlackContext)
 
 if __name__ == '__main__':
-    discord = application.serviceLocator.get(DiscordClient)
     application.run_dev(host="0.0.0.0", port=4000)
